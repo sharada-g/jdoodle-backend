4261138ea82ae0b4039176b15fafbdc3c11de456
sharada-g <gunathilakesharada@gmail.com>
2023-05-01 06:13:54 +1000

    update the backend for jdoodle cors issue!

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require("axios");
 
 const cors = require("cors");
 
@@ -23,6 +24,18 @@ app.use("/steps", stepsRoute);
 app.use("/testimonials", testimonialsRoute);
 app.use("/leaderboard", leaderboardRoute);
 app.use("/challenges", challengesRoute);
+
+app.post("/execute", async (req, res) => {
+  try {
+    const response = await axios.post(
+      "https://api.jdoodle.com/v1/execute",
+      req.body
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
