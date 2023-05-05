96f138349dbcf81ce64fa64afd0456a9aad7dcb4
sharada-g <gunathilakesharada@gmail.com>
2023-05-05 14:05:58 +1000

    remove console logs

--- index.js
+++ index.js
@@ -26,13 +26,12 @@ app.use("/leaderboard", leaderboardRoute);
 app.use("/challenges", challengesRoute);
 
 app.post("/token", async (req, res) => {
-  console.log(req.body);
   try {
     const response = await axios.post(
       "https://api.jdoodle.com/v1/auth-token",
       req.body
     );
-    console.log(response.data);
+
     res.json(response.data);
   } catch (error) {
     res.status(500).json({ error: error.message });
